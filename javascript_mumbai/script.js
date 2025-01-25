a65305bfbcb13eb16c7e14854b5df0196b663afd
Ekansh Rajpal <ekanshrajpal13@gmail.com>
2025-01-25 18:53:26 +0530

    updated code uploading - download from godaddy

---
+++ javascript_mumbai/script.js
@@ -0,0 +1,71 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+var click = 1;
+function clickOn(val) {
+
+    if (val == 1) {
+        document.querySelector(".toggle_switch").innerHTML = `<i class="ri-close-line"></i>`
+        document.querySelector(".items").style.left = "0%";
+        click = 0;
+    }
+    else {
+        document.querySelector(".toggle_switch").innerHTML = `<i class="ri-menu-3-line"></i>`;
+        document.querySelector(".items").style.left = "-110%";
+        click = 1;
+    }
+}
+document.querySelector(".toggle_switch").addEventListener("click", (re) => {
+    clickOn(click);
+})
+
+var item = document.querySelectorAll(".item");
+item.forEach((i) => {
+    i.addEventListener("click", function () {
+        clickOn(0)
+    })
+})
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // effect: "coverflow",
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+    parallax: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+});
